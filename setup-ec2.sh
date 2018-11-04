ed50ff3bda4887da9328da2d54e0fc59ff46f9e2
Peter Chow <peteroid.howinai@gmail.com>
2018-11-05 02:36:11 +0800

    update setup script

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo "Updating yum ..."
-sudo yum update
+sudo yum -y update
 
 echo "Installing node ..."
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
 echo "Installing yarn"
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-sudo yum install yarn
+sudo yum -y install yarn
 
 echo "Creating a local npm global directory"
 mkdir ~/.npm-global
