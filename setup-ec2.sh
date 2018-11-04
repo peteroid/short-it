01a713241fab3e3a11e0e28ade8b43b0a0844e18
Peter Chow <peteroid.howinai@gmail.com>
2018-11-05 01:51:12 +0800

    - add more libs - add ec2 setup script

---
+++ setup-ec2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Updating yum ..."
+sudo yum update
+
+echo "Installing node ..."
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+
+echo "Installing yarn"
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install yarn
+
+echo "Creating a local npm global directory"
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installing pm2"
+npm i pm2@latest -g
